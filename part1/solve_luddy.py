fef68551a7047f58137122a90920b99fd78c22b7
Kaustubh <kbhaler@iu.edu>
2019-10-08 23:49:57 -0400

    solveLuddy working for 1-2

--- part1/solve_luddy.py
+++ part1/solve_luddy.py
@@ -1,74 +1,159 @@
 #!/usr/local/bin/python3
 # solve_luddy.py : Sliding tile puzzle solver
 #
-# Code by: [PLEASE PUT YOUR NAMES AND USER IDS HERE]
+# Code by: [Kaustubh Bhalerao(kbhaler), Suyash Poredi(sporedi), Dipali Tolia(dtolia)]
 #
 # Based on skeleton code by D. Crandall, September 2019
 #
 from queue import PriorityQueue
+import queue as Q
 import sys
+import time
+import os
+import heapq
+from heapq import heapify, heappush, heappop
 
-MOVES = { "R": (0, -1), "L": (0, 1), "D": (-1, 0), "U": (1,0) }
+MOVES = {"R": (0, -1), "L": (0, 1), "D": (-1, 0), "U": (1, 0)}
+
+# LUDDY_MOVES = {"A": (2, 1), "B": (2, -1), "C": (-2, 1), "D": (-2, -1), "E": (1, 2), "F": (1, -2), "G": (-1, 2),"H": (-1, -2)}
+
+LUDDY_MOVES = {"A": (-2, -1), "B": (-2, 1), "C": (2, -1), "D": (2, 1), "E": (-1, -2), "F": (-1, 2), "G": (1, -2),
+               "H": (1, 2)}
+
 
 def rowcol2ind(row, col):
-    return row*4 + col
+    return row * 4 + col
+
 
 def ind2rowcol(ind):
-    return (int(ind/4), ind % 4)
+    return (int(ind / 4), ind % 4)
+
 
 def valid_index(row, col):
     return 0 <= row <= 3 and 0 <= col <= 3
 
+
 def swap_ind(list, ind1, ind2):
-    return list[0:ind1] + (list[ind2],) + list[ind1+1:ind2] + (list[ind1],) + list[ind2+1:]
+    return list[0:ind1] + (list[ind2],) + list[ind1 + 1:ind2] + (list[ind1],) + list[ind2 + 1:]
+
 
 def swap_tiles(state, row1, col1, row2, col2):
-    return swap_ind(state, *(sorted((rowcol2ind(row1,col1), rowcol2ind(row2,col2)))))
+    return swap_ind(state, *(sorted((rowcol2ind(row1, col1), rowcol2ind(row2, col2)))))
+
 
 def printable_board(row):
-    return [ '%3d %3d %3d %3d'  % (row[j:(j+4)]) for j in range(0, 16, 4) ]
+    return ['%3d %3d %3d %3d' % (row[j:(j + 4)]) for j in range(0, 16, 4)]
 
-# return a list of possible successor states
-def successors(state):
+
+# Finding the number of unorganized tiles on the board
+def unorganized_tiles(state):
+    count = 0
+    # print(state)
+    for i in range(15):
+        if state[i] != (i + 1):
+            count += 1
+    if state[15] != 0:
+        count += 1
+    return count
+
+
+# Checks the board if it is solvable or not
+def issolvable(state):
+    inversion = 0
+
+    for i in range(len(state)):
+        for j in range(i, len(state)):
+            if (state[i] > state[j]) and state[i] != 0 and state[j] != 0:
+                inversion += 1
     (empty_row, empty_col) = ind2rowcol(state.index(0))
-    return [ (swap_tiles(state, empty_row, empty_col, empty_row+i, empty_col+j), c) \
-             for (c, (i, j)) in MOVES.items() if valid_index(empty_row+i, empty_col+j) ]
 
-# check if we've reached the goal
-def is_goal(state):
-    return sorted(state[:-1]) == list(state[:-1]) and state[-1]==0
-    
-# The solver! - using BFS right now
-def solve(initial_board):
-    fringe = [ (initial_board, "") ]
-    while len(fringe) > 0:
-        (state, route_so_far) = fringe.pop()
-        for (succ, move) in successors( state ):
-            if is_goal(succ):
-                return( route_so_far + move )
-            fringe.insert(0, (succ, route_so_far + move ) )
+    #print("Inv: ", inversion)
+    #print("\n")
+
+    if empty_row % 2 == 0 and inversion % 2 == 1:
+        return True
+    if empty_row % 2 == 1 and inversion % 2 == 0:
+        return True
+
     return False
 
-# test cases
+
+# Finding the possible successor states
+def successors(state):
+    # Finding the coordinates of '0'
+    (empty_row, empty_col) = ind2rowcol(state.index(0))
+    if sys.argv[2] == 'original':
+        return [(swap_tiles(state, empty_row, empty_col, empty_row + i, empty_col + j), c) \
+                for (c, (i, j)) in MOVES.items() if valid_index(empty_row + i, empty_col + j)]
+    elif sys.argv[2] == 'luddy':
+
+        return [(swap_tiles(state, empty_row, empty_col, empty_row + i, empty_col + j), c) \
+                for (c, (i, j)) in LUDDY_MOVES.items() if valid_index(empty_row + i, empty_col + j)]
+    else:
+        return [(swap_tiles(state, empty_row, empty_col, (empty_row + i) % 4, (empty_col + j) % 4), c) for (c, (i, j))
+                in MOVES.items()]
+
+
+# checking if we have reached goal state or not
+def is_goal(state):
+    return sorted(state[:-1]) == list(state[:-1]) and state[-1] == 0
+
+
+# Solve method - using A* Search
+def solve(initial_board):
+    fringe = [(initial_board, "")]
+    closed = []
+
+    q = Q.PriorityQueue()
+    cost = 0
+    g_cost = 1
+    h_cost = unorganized_tiles(initial_board)
+    q.put(((h_cost + g_cost), initial_board, ""))
+
+    while not q.empty():
+        (f, state, route_so_far) = q.get()
+        closed.append(state)
+        if is_goal(state):
+            return route_so_far
+
+        for (succ, move) in successors(state):
+            if succ in closed: continue
+            h_cost = unorganized_tiles(succ)
+            q.put(((h_cost + g_cost), succ, route_so_far + move))
+            g_cost += 1
+
+    return False
+
+
+# testing cases
 if __name__ == "__main__":
-    if(len(sys.argv) != 3):
-        raise(Exception("Error: expected 2 arguments"))
+    if len(sys.argv) != 3:
+        raise (Exception("Error: expected 2 arguments"))
 
     start_state = []
+
     with open(sys.argv[1], 'r') as file:
         for line in file:
-            start_state += [ int(i) for i in line.split() ]
+            start_state += [int(i) for i in line.split()]
 
+    '''
     if(sys.argv[2] != "original"):
         raise(Exception("Error: only 'original' puzzle currently supported -- you need to implement the other two!"))
-
+    '''
     if len(start_state) != 16:
-        raise(Exception("Error: couldn't parse start state file"))
+        raise (Exception("Error: couldn't parse start state file"))
 
-    print("Start state: \n" +"\n".join(printable_board(tuple(start_state))))
+    start_state = tuple(start_state)
 
-    print("Solving...")
-    route = solve(tuple(start_state))
-    
-    print("Solution found in " + str(len(route)) + " moves:" + "\n" + route)
+    print("Start state: \n" + "\n".join(printable_board(start_state)))
 
+    if issolvable(start_state):
+
+        print("Solving...")
+
+        route = solve(tuple(start_state))
+
+        print("Solution found in " + str(len(route)) + " moves:" + "\n" + route)
+
+    else:
+        print("Inf")
--- part3/choose_team.py
+++ part3/choose_team.py
@@ -2,7 +2,7 @@
 #
 # choose_team.py : Choose a team of maximum skill under a fixed budget
 #
-# Code by: [PLEASE PUT YOUR NAMES AND USER IDS HERE]
+# Code by: [Kaustubh Bhalerao(kbhaler), Suyash Poredi(sporedi), Dipali Tolia(dtolia)]
 #
 # Based on skeleton code by D. Crandall, September 2019
 #
