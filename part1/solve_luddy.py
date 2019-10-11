776da574a5d1300039611afca532b88463ba1146
Kaustubh <kbhaler@iu.edu>
2019-10-10 23:31:20 -0400

    Optimized Part 1 for Luddy Case

--- part1/solve_luddy.py
+++ part1/solve_luddy.py
@@ -15,18 +15,16 @@ from heapq import heapify, heappush, heappop
 
 MOVES = {"R": (0, -1), "L": (0, 1), "D": (-1, 0), "U": (1, 0)}
 
-# LUDDY_MOVES = {"A": (2, 1), "B": (2, -1), "C": (-2, 1), "D": (-2, -1), "E": (1, 2), "F": (1, -2), "G": (-1, 2),"H": (-1, -2)}
+LUDDY_MOVES = {"A": (2, 1), "B": (2, -1), "C": (-2, 1), "D": (-2, -1), "E": (1, 2), "F": (1, -2), "G": (-1, 2),"H": (-1, -2)}
 
-LUDDY_MOVES = {"A": (-2, -1), "B": (-2, 1), "C": (2, -1), "D": (2, 1), "E": (-1, -2), "F": (-1, 2), "G": (1, -2),
-               "H": (1, 2)}
-
+#LUDDY_MOVES = {"A": (-2, -1), "B": (-2, 1), "C": (2, -1), "D": (2, 1), "E": (-1, -2), "F": (-1, 2), "G": (1, -2), "H": (1, 2)}
 
 def rowcol2ind(row, col):
     return row * 4 + col
 
 
 def ind2rowcol(ind):
-    return (int(ind / 4), ind % 4)
+    return int(ind / 4), ind % 4
 
 
 def valid_index(row, col):
@@ -47,14 +45,13 @@ def printable_board(row):
 
 # Finding the number of unorganized tiles on the board
 def unorganized_tiles(state):
-    count = 0
-    # print(state)
+    count_tiles = 0
     for i in range(15):
         if state[i] != (i + 1):
-            count += 1
+            count_tiles += 1
     if state[15] != 0:
-        count += 1
-    return count
+                count_tiles += 1
+    return count_tiles
 
 
 # Checks the board if it is solvable or not
@@ -66,9 +63,6 @@ def issolvable(state):
             if (state[i] > state[j]) and state[i] != 0 and state[j] != 0:
                 inversion += 1
     (empty_row, empty_col) = ind2rowcol(state.index(0))
-
-    #print("Inv: ", inversion)
-    #print("\n")
 
     if empty_row % 2 == 0 and inversion % 2 == 1:
         return True
@@ -82,16 +76,24 @@ def issolvable(state):
 def successors(state):
     # Finding the coordinates of '0'
     (empty_row, empty_col) = ind2rowcol(state.index(0))
-    if sys.argv[2] == 'original':
-        return [(swap_tiles(state, empty_row, empty_col, empty_row + i, empty_col + j), c) \
-                for (c, (i, j)) in MOVES.items() if valid_index(empty_row + i, empty_col + j)]
-    elif sys.argv[2] == 'luddy':
+    if sys.argv[2] == 'luddy':
 
         return [(swap_tiles(state, empty_row, empty_col, empty_row + i, empty_col + j), c) \
                 for (c, (i, j)) in LUDDY_MOVES.items() if valid_index(empty_row + i, empty_col + j)]
+
+    elif sys.argv[2] == 'original':
+        return [(swap_tiles(state, empty_row, empty_col, empty_row + i, empty_col + j), c) \
+                for (c, (i, j)) in MOVES.items() if valid_index(empty_row + i, empty_col + j)]
     else:
         return [(swap_tiles(state, empty_row, empty_col, (empty_row + i) % 4, (empty_col + j) % 4), c) for (c, (i, j))
                 in MOVES.items()]
+
+#Fvalue is calculated based on number of misplaced tiles and length of move
+def f_cost(succ, move):
+    h_cost = unorganized_tiles(succ)
+    length_move = len(move)
+    f = h_cost + length_move
+    return f
 
 
 # checking if we have reached goal state or not
@@ -99,28 +101,30 @@ def is_goal(state):
     return sorted(state[:-1]) == list(state[:-1]) and state[-1] == 0
 
 
-# Solve method - using A* Search
+# Solve method - using A* Search, Defining heuristic and F value
 def solve(initial_board):
-    fringe = [(initial_board, "")]
-    closed = []
+    fringe = [(0, initial_board, "")]
+    closed = set()
 
     q = Q.PriorityQueue()
-    cost = 0
-    g_cost = 1
+    g_cost = 0
     h_cost = unorganized_tiles(initial_board)
-    q.put(((h_cost + g_cost), initial_board, ""))
+    f = h_cost + 0
+    q.put((f, initial_board, "",0))
 
     while not q.empty():
-        (f, state, route_so_far) = q.get()
-        closed.append(state)
+        (f, state, route_so_far,g) = q.get()
+        g=g+1
+
         if is_goal(state):
             return route_so_far
 
         for (succ, move) in successors(state):
-            if succ in closed: continue
-            h_cost = unorganized_tiles(succ)
-            q.put(((h_cost + g_cost), succ, route_so_far + move))
-            g_cost += 1
+            if succ not in closed:
+                closed.add(succ)
+                f_value = f_cost(succ, move)
+                q.put((f_value + g, succ, route_so_far + move, g))
+
 
     return False
 
